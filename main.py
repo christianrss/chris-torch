5ecccf8c8c6bc8318a1881aac279acdb11b4f4fb
Christian Rafael <christianparadix@gmail.com>
2026-01-24 22:57:36 -0300

    feat: Helper Function: Implementing to_array

--- main.py
+++ main.py
@@ -6,11 +6,17 @@ class Var:
             raise TypeError("The value must be a ndarray")
         self.value = value
 
+def to_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
 class Function:
     def __call__(self, x):
         x_value = x.value
         y_value = self.forward(x_value)
-        y = Var(np.array(y_value))
+        y_value = to_array(y_value)
+        y = Var(y_value)
         return y
 
     def forward(self, x_value):
@@ -22,6 +28,10 @@ class Sin(Function):
 
 def sin(x):
     return Sin()(x)
+
+# x = np.array(2)
+# y = to_array(np.square(x))
+# print(y, type(y))
 
 x = Var(np.array(np.pi/2))
 y = sin(x)
